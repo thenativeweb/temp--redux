ce81f5800eba41dc913476ca7d5578a5845580e7
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2022-07-21 11:29:55 +0200

    Add redux-toolkit and rewrite reducer

--- src/store/setupStore.ts
+++ src/store/setupStore.ts
@@ -1,31 +1,16 @@
-import { Action, createStore } from 'redux';
-import { addTransaction, revokeTransaction, transactionLogReducer } from './slices/transactionLog';
-import { Transaction } from './types/Transaction';
-
-interface AppState {
-  transactions: Transaction[];
-}
-
-const initialState: AppState = {
-  transactions: []
-};
-
-const rootReducer = function (
-  state: AppState = initialState,
-  action: Action
-): AppState {
-  if (action.type.startsWith('transactionLog/')) {
-    return transactionLogReducer(state, action as any);
-  } else {
-    return state;
-  }
-}
+import { configureStore } from '@reduxjs/toolkit';
+import transactionLog from './slices/transactionLog';
 
 const setupStore = function () {
-  return createStore(rootReducer);
+  return configureStore({
+    reducer: {
+      transactionLog
+    }
+  });
 }
 
 type AppStore = ReturnType<typeof setupStore>;
+type AppState = ReturnType<AppStore['getState']>;
 type Dispatch = AppStore['dispatch'];
 
 export type {
@@ -34,7 +19,5 @@ export type {
   Dispatch
 }
 export {
-  setupStore,
-  addTransaction,
-  revokeTransaction,
+  setupStore
 };
--- src/store/slices/transactionLog.ts
+++ src/store/slices/transactionLog.ts
@@ -1,50 +1,34 @@
-import { AppState } from '../setupStore';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Transaction } from '../types/Transaction';
 
-interface AddTransactionAction {
-  type: 'transactionLog/ADD_TRANSACTION';
-  payload: {
-    transaction: Transaction;
-  };
+interface TransactionLogState {
+  transactions: Transaction[];
 }
 
-interface RevokeTransactionAction {
-  type: 'transactionLog/REVOKE_TRANSACTION',
-  payload: {
-    transactionId: string;
-  };
-}
+const initialState: TransactionLogState = {
+  transactions: []
+};
 
-const addTransaction = (payload: AddTransactionAction['payload']): AddTransactionAction => ({
-  type: 'transactionLog/ADD_TRANSACTION',
-  payload
-});
-
-const revokeTransaction = (payload: RevokeTransactionAction['payload']): RevokeTransactionAction => ({
-  type: 'transactionLog/REVOKE_TRANSACTION',
-  payload
-});
-
-const transactionLogReducer = (state: AppState, action: AddTransactionAction | RevokeTransactionAction): AppState => {
-  switch (action.type) {
-    case 'transactionLog/ADD_TRANSACTION':
+const transactionLogSlice = createSlice({
+  name: 'transactionLog',
+  initialState,
+  reducers: {
+    addTransaction: (
+      state,
+      action: PayloadAction<{ transaction: Transaction }>
+    ) => {
       return {
         transactions: [ ...state.transactions, action.payload.transaction ]
       };
-
-    case 'transactionLog/REVOKE_TRANSACTION':
-      const { transactionId } = action.payload;
-
-      return {
-        transactions: state.transactions.filter(
-          transaction => transaction.transactionId !== transactionId
-        )
-      };
+    }
   }
-};
+});
+
+const {
+  addTransaction
+} = transactionLogSlice.actions;
 
 export {
-  addTransaction,
-  revokeTransaction,
-  transactionLogReducer,
+  addTransaction
 };
+export default transactionLogSlice.reducer;
