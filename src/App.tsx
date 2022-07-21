5684c2ac23c6aaf40a24b6c1cfc87ab30caddfe5
Noah Hummel <noah.hummel@thenativeweb.io>
2022-07-21 10:32:06 +0200

    Added slices and action creators

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppState, Dispatch } from './store/setupStore';
+import { addTransaction, AppState, Dispatch, revokeTransaction } from './store/setupStore';
 
 function App() {
   const dispatch = useDispatch<Dispatch>();
   const transactions = useSelector((state: AppState) => state.transactions);
 
-  const handleButtonClick = function () {
-    dispatch({
-      type: 'ADD_TRANSACTION',
-      payload: {
-        transaction: {
-          fromAccount: 'hannes',
-          toAccount: 'noah',
-          amount: 1337
-        }
+  const handleAddTransactionButtonClicked = function () {
+    dispatch(addTransaction({
+      transaction: {
+        transactionId: 'test',
+        fromAccount: 'hannes',
+        toAccount: 'noah',
+        amount: 1337
       }
-    })
-  }
+    }));
+  };
+  const handleRevokeTransactionButtonClicked = function () {
+    dispatch(revokeTransaction({
+      transactionId: 'test'
+    }));
+  };
 
   return (
     <div className="App">
@@ -27,7 +30,8 @@ function App() {
           From: {transaction.fromAccount} To: {transaction.toAccount} Amount: {transaction.amount}
         </li>))}
       </ul>
-      <button onClick={handleButtonClick}>Add transaction!</button>
+      <button onClick={handleAddTransactionButtonClicked}>Add transaction!</button>
+      <button onClick={handleRevokeTransactionButtonClicked}>Add transaction!</button>
     </div>
   );
 }
--- src/store/setupStore.ts
+++ src/store/setupStore.ts
@@ -1,17 +1,35 @@
 import { createStore, Action } from 'redux';
 
 interface Transaction {
+  transactionId: string;
   fromAccount: string;
   toAccount: string;
   amount: number;
 }
 
 interface AddTransactionAction {
-  type: 'ADD_TRANSACTION';
+  type: 'transactionLog/ADD_TRANSACTION';
   payload: {
     transaction: Transaction;
-  }
+  };
 }
+
+interface RevokeTransactionAction {
+  type: 'transactionLog/REVOKE_TRANSACTION',
+  payload: {
+    transactionId: string;
+  };
+}
+
+const addTransaction = (payload: AddTransactionAction['payload']): AddTransactionAction => ({
+  type: 'transactionLog/ADD_TRANSACTION',
+  payload
+});
+
+const revokeTransaction = (payload: RevokeTransactionAction['payload']): RevokeTransactionAction => ({
+  type: 'transactionLog/REVOKE_TRANSACTION',
+  payload
+});
 
 interface TransactionLogState {
   transactions: Transaction[];
@@ -21,24 +39,38 @@ const initialState: TransactionLogState = {
   transactions: []
 };
 
-const transactionLogReducer = function (
-  state: TransactionLogState = initialState,
-  action: AddTransactionAction
-): TransactionLogState {
+
+const transactionLogReducer = (state: TransactionLogState, action: AddTransactionAction | RevokeTransactionAction): TransactionLogState => {
   switch (action.type) {
-    case 'ADD_TRANSACTION': {
+    case 'transactionLog/ADD_TRANSACTION':
       return {
         transactions: [ ...state.transactions, action.payload.transaction ]
-      }
-    }
-    default: {
-      return state;
-    }
+      };
+
+    case 'transactionLog/REVOKE_TRANSACTION':
+      const { transactionId } = action.payload;
+
+      return {
+        transactions: state.transactions.filter(
+          transaction => transaction.transactionId !== transactionId
+        )
+      };
+  }
+}
+
+const rootReducer = function (
+  state: TransactionLogState = initialState,
+  action: AddTransactionAction | RevokeTransactionAction
+): TransactionLogState {
+  if (action.type.startsWith('transactionLog/')) {
+    return transactionLogReducer(state, action);
+  } else {
+    return state;
   }
 }
 
 const setupStore = function () {
-  return createStore(transactionLogReducer);
+  return createStore(rootReducer);
 }
 
 type AppStore = ReturnType<typeof setupStore>;
@@ -51,5 +83,7 @@ export type {
   Dispatch
 }
 export {
-  setupStore
+  setupStore,
+  addTransaction,
+  revokeTransaction,
 };
